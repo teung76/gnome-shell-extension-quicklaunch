4265f8a710a9fd7af679523a556b996d47a34f7b
teung76 <hong.tin.eng@gmail.com>
2023-12-12 21:47:47 +0800

    Upgrade and fix for latest gnome shell 3.38 and 40+

--- QuickLaunch@github.com/extension.js
+++ QuickLaunch@github.com/extension.js
@@ -1,14 +1,13 @@
 /* vim: ts=4 sw=4
  */
-const Gio = imports.gi.Gio;
-const GLib = imports.gi.GLib;
-const Gtk = imports.gi.Gtk;
 
-const St = imports.gi.St;
+const GETTEXT_DOMAIN = "QuickLaunch-extension";
+
+const { Atk, Gio, GLib, Gtk, GObject, Shell, St } = imports.gi;
+const ExtensionUtils = imports.misc.extensionUtils;
+
 const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
-const Shell = imports.gi.Shell;
-const Atk = imports.gi.Atk;
 
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
@@ -16,42 +15,38 @@ const Lang = imports.lang;
 const FileUtils = imports.misc.fileUtils;
 const Util = imports.misc.util;
 
+const Me = ExtensionUtils.getCurrentExtension();
+
+const Gettext = imports.gettext.domain(GETTEXT_DOMAIN);
+const _ = Gettext.gettext;
+
 const AppsPath = GLib.get_home_dir() + '/.local/share/gnome-shell/quicklaunch';
 const AppsPaths = [ GLib.get_home_dir() + '/.local/user/apps', AppsPath ];
 
 const IndicatorName = 'QuickLaunch';
 
-/**
- * Gicon Menu Item Object
- */
-function PopupGiconMenuItem() {
-    this._init.apply(this, arguments);
-}
-
-PopupGiconMenuItem.prototype = {
-    __proto__: PopupMenu.PopupBaseMenuItem.prototype,
-
-    _init: function (text, gIcon, params) {
-        PopupMenu.PopupBaseMenuItem.prototype._init.call(this, params);
+const PopupGiconMenuItem = GObject.registerClass(
+class PopupGiconMenuItem extends PopupMenu.PopupBaseMenuItem {
+    _init(text, gIcon, params) {
+        super._init(params);
 
         this.label = new St.Label({ text: text });
         this._icon = new St.Icon({
                 gicon: gIcon,
-                style_class: 'popup-menu-icon' });
+                style_class: 'popup-menu-icon'
+            });
         this.actor.add_child(this._icon, { align: St.Align.END });
         this.actor.add_child(this.label);
-    },
-};
+    }
+});
 
 /**
  * QuickLaunch Object
  */
-const QuickLaunch = new Lang.Class({
-    Name: IndicatorName,
-    Extends: PanelMenu.Button,
-
-    _init: function(metadata, params) {
-        this.parent(null, IndicatorName);
+const QuickLaunch = GObject.registerClass(
+class QuickLaunch extends PanelMenu.Button {
+    _init(metadata, params) {
+        super._init(0.0, _(IndicatorName));
         this.actor.accessible_role = Atk.Role.TOGGLE_BUTTON;
 
         this._icon = new St.Icon({ icon_name: 'system-run-symbolic', style_class: 'system-status-icon' }); 
@@ -62,39 +57,39 @@ const QuickLaunch = new Lang.Class({
         this._setupDirectory();
         this._setupAppMenuItems();
         this._setupNewEntryDialog();
-        this._setupDirectoryMonitor();
-    },
+//        this._setupDirectoryMonitor();
+    }
 
-    _onDestroy: function() {
-        this._monitor.cancel();
+    _onDestroy() {
+//        this._monitor.cancel();
         Mainloop.source_remove(this._appDirectoryTimeoutId);
-    },
+    }
 
     /**
      * create dir unless exists
      */
-    _setupDirectory: function() {
+    _setupDirectory() {
         let dir = Gio.file_new_for_path(AppsPath);
         if (!dir.query_exists(null)) {
             global.log('create dir ' + AppsPath );
             dir.make_directory_with_parents(null);
         }
         this._appDirectory = dir;
-    },
+    }
 
     /**
      * reload the menu
      */
-    _reloadAppMenu: function() {
+    _reloadAppMenu() {
         this.menu.removeAll();
         this._setupAppMenuItems();
         this._setupNewEntryDialog();
-    },
+    }
 
     /**
      * change directory monitor, see placeDisplay.js
      */
-    _setupDirectoryMonitor: function() {
+    _setupDirectoryMonitor() {
         if (!this._appDirectory.query_exists(null))
             return;
         this._monitor = this._appDirectory.monitor_directory(Gio.FileMonitorFlags.NONE, null);
@@ -109,20 +104,20 @@ const QuickLaunch = new Lang.Class({
                 return false;
             }));
         }));
-    },
+    }
 
     /**
      * setup menu items for all desktop files
      */
-    _setupAppMenuItems: function(path) {
+    _setupAppMenuItems(path) {
         for (let path in AppsPaths)
             this._createDefaultApps(AppsPaths[path]);
-    },
+    }
 
     /**
      * load desktop files from a directory
      */
-    _createDefaultApps: function(path) {
+    _createDefaultApps(path) {
         let _appsDir = Gio.file_new_for_path(path);
         if (!_appsDir.query_exists(null)) {
             global.log('App path ' + path + ' could not be opened!');
@@ -152,12 +147,12 @@ const QuickLaunch = new Lang.Class({
             }
         }
         fileEnum.close(null);
-    },
+    }
 
     /**
      * add menu item to popup
      */
-    _addAppItem: function(desktopPath) {
+    _addAppItem(desktopPath) {
         // from http://www.roojs.com/seed/gir-1.2-gtk-3.0/gjs/
         let appInfo = Gio.DesktopAppInfo.new_from_filename(desktopPath);
         if (!appInfo) {
@@ -181,24 +176,24 @@ const QuickLaunch = new Lang.Class({
         });
         this.menu.addMenuItem(menuItem, pos);
         return menuItem;
-    },
+    }
 
     /**
      * create popoup menu item with callback
      */
-    _createAppItem: function(appInfo, callback) {
+    _createAppItem(appInfo, callback) {
         let menuItem = new PopupGiconMenuItem(appInfo.get_name(), appInfo.get_icon(), {});
         menuItem.connect('activate', Lang.bind(this, function (menuItem, event) {
                     callback(menuItem, event);
         }));
 
         return menuItem;
-    },
+    }
 
     /**
      * add "new app"-dialog link to popup menu
      */
-    _setupNewEntryDialog: function() {
+    _setupNewEntryDialog() {
         let entryCreator = new DesktopEntryCreator();
         if (! entryCreator.hasEditor()) {
             global.log('gnome-desktop-item-edit is not installed!');
@@ -213,8 +208,7 @@ const QuickLaunch = new Lang.Class({
             entryCreator.createEntry(AppsPath);
         }));
         this.menu.addMenuItem(item);
-    },
-
+    }
 });
 
 /**
@@ -222,35 +216,34 @@ const QuickLaunch = new Lang.Class({
  * 
  * use gnome-dekstop-item-edit to create a new desktop entry file
  */
-const DesktopEntryCreator = new Lang.Class({
-    Name: 'DesktopEntryCreator',
+class DesktopEntryCreator {
+    constructor() {
+        let gdie = Gio.file_new_for_path('/usr/bin/gnome-desktop-item-edit');
+	this._existEditor = gdie.query_exists(null);
+    }
 
-    hasEditor: function() {
-        let _gdie = Gio.file_new_for_path('/usr/bin/gnome-desktop-item-edit');
-        if (!_gdie.query_exists(null)) {
-            return false;
-        } 
-        return true;
-    },
+    hasEditor() {
+        return this._existEditor;
+    }
 
-    createEntry: function(destination) {
+    createEntry(destination) {
         Util.trySpawn( ['gnome-desktop-item-edit', this._getNewEntryName(destination) ]);
-    },
+    }
 
-    _getNewEntryName: function(destination) {
+    _getNewEntryName(destination) {
         return GLib.build_filenamev([destination, this._createUUID() + '.desktop']);
-    },
+    }
 
     /*
      * thanks to stackoverflow:
      */
-    _createUUID: function() {
+    _createUUID() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
             return v.toString(16);
         });
-    },
-});
+    }
+};
 
 /**
  * Extension Setup
@@ -262,7 +255,7 @@ let _indicator;
 
 function enable() {
     _indicator = new QuickLaunch();
-    Main.panel.addToStatusArea(IndicatorName, _indicator);
+    Main.panel.addToStatusArea(_(IndicatorName), _indicator, 1, "left");
 }
 
 function disable() {
